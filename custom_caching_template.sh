ba981e3cd8f7281f715da4685bd0af9ea5ae1e96
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-09-09 18:51:38 +0200

    Update custom_caching_template.sh

--- custom_caching_template.sh
+++ custom_caching_template.sh
@@ -2,9 +2,13 @@
 
 mv /usr/local/vesta/data/templates/web/nginx/caching.tpl /usr/local/vesta/data/templates/web/nginx/caching.tpl_OLD
 
+mv /usr/local/vesta/data/templates/web/nginx/caching.stpl /usr/local/vesta/data/templates/web/nginx/caching.stpl_OLD
+
 cd /usr/local/vesta/data/templates/web/nginx/
 
 wget https://raw.githubusercontent.com/Wamphyre/GNULinuxTools/master/caching.tpl
+
+wget https://raw.githubusercontent.com/Wamphyre/GNULinuxTools/master/caching.stpl
 
 cd
 
