334c2af289b480e74e5429ecdb161dc94f10280b
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-09-09 17:59:34 +0200

    Create CSF_vesta_installer.sh
    CSF Module installer for Vesta Control Panel

---
+++ CSF_vesta_installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget https://vestacp.ss88.uk/Install_CSF_on_VestaCP/Install.sh -O ./CSF_Installer.sh
+
+chmod 777 ./CSF_Installer.sh
+
+./CSF_Installer.sh
+
+echo ""
+
+echo "CSF Module for Vesta installed"
+
